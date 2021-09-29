3942a7db22b4d9d27f4e07c4c8439510c57c165e
djmaster458 <djmaster458@gmail.com>
2021-09-29 15:10:44 -0400

    Exercise 1.2

--- src/lesson01/src/uart.c
+++ src/lesson01/src/uart.c
@@ -5,19 +5,23 @@
 void uart_send ( char c )
 {
 	while(1) {
-		if(get32(AUX_MU_LSR_REG)&0x20) 
+		if(get32(UART_FR) & 0x20u) //wait for TX ready
+			continue;
+		else
 			break;
 	}
-	put32(AUX_MU_IO_REG,c);
+	put32(UART_DR,c); //Put data into data reg
 }
 
 char uart_recv ( void )
 {
 	while(1) {
-		if(get32(AUX_MU_LSR_REG)&0x01) 
+		if(get32(UART_FR) & 0x10u) //Wait for RX Ready
+			continue;
+		else
 			break;
 	}
-	return(get32(AUX_MU_IO_REG)&0xFF);
+	return(get32(UART_DR) & 0xFF);
 }
 
 void uart_send_string(char* str)
@@ -30,7 +34,6 @@ void uart_send_string(char* str)
 void uart_init ( void )
 {
 	unsigned int selector;
-	unsigned int baud_rate = BAUD_RATE_9600;
 
 	selector = get32(GPFSEL1);
 	selector &= ~(7<<12);                   // clean gpio14
@@ -45,6 +48,18 @@ void uart_init ( void )
 	delay(150);
 	put32(GPPUDCLK0,0);
 
-	unsigned int uart_int_divisor = 0xFFFF & (UART_CLK / (16 * baud_rate)); //Page 56 of PrimeCell PL011
-	unsigned int FBRD = 0x3f & (UART_CLK % (16 * baud_rate) * 64 / UART_CLK);
+	//Assume Using 115200
+	//BRDI = CLOCK / (16 * BAUD)
+	//BRDF = integer((fractional part of above * 64) + 0.5)
+	unsigned int uart_int_divisor = 26; //Page 56 of PrimeCell PL011
+	unsigned int uart_fract_divisior = 3;
+
+	put32(UART_IBRD, uart_int_divisor);
+	put32(UART_FBRD, uart_fract_divisior);
+
+	put32(UART_LCRH, (3u << 5)); //Set Word Length to 8-bits by Enabling WLEN to 11
+
+	unsigned int uart_cr = 1u | (1u << 8) | (1u << 9); //Enable UART, TX, and RX
+
+	put32(UART_CR, uart_cr);
 }
